03c35a138375a04d8d9e200b221e0034840ac5a4
Gabi Moura <gabi.silva.moura@gmail.com>
2016-09-24 16:42:51 -0400

    Sign up and sign out added

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,8 @@
 class TopicsController < ApplicationController
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate_user!, except: [:index,:show]
+
   # GET /topics
   # GET /topics.json
   def index
